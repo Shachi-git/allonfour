4eabd0b40e7c7c0a96ed3d19621430544c35c5a5
Shachi-git <delacruz.johnpaulstar@gmail.com>
2025-09-18 22:14:56 +0800

    AOF-edit file name

--- src/components/body/InfoSection.vue
+++ src/components/body/InfoSection.vue
@@ -6,26 +6,26 @@ const sections = [
         title: `Create a <span class="text-highlight-blue">Personalized</span> Plan with Your Implant Surgeon!`,
         content: `We're all unique, so creating a Personalized Treatment Plan for your smile is the most important step towards getting the smile of your dreams! Your dental implant surgeon will meet with you and start this process on day one. S/he listens to your story, your wishes, the functionality you've lost, any pain you've been feeling, etc. It's our time to listen and absorb every detail 
     in a safe, nurturing, non-rushed, and empathetic environment. If you have photos of the smile you want, great! We have many no-dentures, same-day All-on-4™ treatment options, so if budget is a factor, let us know, we'll work with you. Botched job? We'll fix that. The idea is to creatively personalize your plan so All-on-4™ SAME DAY dental implants, which avoids dentures, is an option for you.`,
-        image: "/Perio_banner-image-1.png",
+        image: new URL("/Perio_banner-image-1.png", import.meta.url).href,
         reverse: true
     },
     {
         title: `Your Treatment <span class="text-highlight-blue">Counselor</span> Makes This Easy`,
         content: `Next, you'll meet your dedicated Treatment Counselor, your friendly point person who will take care of you all throughout your dental implant journey.
 She aims to make every step easy and to ensure your total comfort throughout the process.`,
-        image: "/doctor-and-patient-2.png",
+        image: new URL("/doctor-and-patient-2.png", import.meta.url).href,
         reverse: false
     },
     {
         title: `Create Your <span class="text-highlight-blue">3D Smile Design</span> with Your Surgeon`,
         content: `Next, we do a simple 3D image that allows us to see your jawbone and create 3D models of your teeth in our in-house lab. That's right, you won't have to travel elsewhere. During a follow-up appointment, you'll get to see your new smile and you and your doctor can tweak it to your liking with our 3D computer-aided software, choose color shades, etc. If you are traveling long distances or need to shorten the process, we are able to accommodate you. Then, you are ready for your new smile!`,
-        image: "/3D-smile-design.png",
+        image: new URL("/3D-smile-design.png", import.meta.url).href,
         reverse: true
     },
     {
         title: `Go home with your new <span class="text-highlight-blue">smile</span> today!`,
         content: `Next, you'll meet your dedicated Treatment Counselor, your friendly point person who will take care of you all throughout your dental implant journey. S/he has many years of dental implant and All-on-4™ experience and will answer any questions, handle any issues, keep you informed, deal with finances, and coordinate your treatment plan with you and your doctor from start to finish. S/he aims to make every step easy and to ensure your total comfort throughout the process. Your Treatment Counselor also plays a big role in Personalizing your Plan and will do everything possible to make All-on-4™ possible for you from financing to 3D smile design.`,
-        image: "/doctor-and-patient-3.png",
+        image: new URL("/doctor-and-patient-3.png", import.meta.url).href,
         reverse: false
     }
 ];
